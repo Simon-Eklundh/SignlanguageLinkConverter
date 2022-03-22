afa54bcdb5de6a17d531c18e813ee7b6b3c72906
Simon <simon.eklundh@gmail.com>
2022-03-22 11:50:46 +0100

    Done everything but the conversion from links/numbers to numbers

--- src/main/java/com/example/signlanguageconverter/Database.java
+++ src/main/java/com/example/signlanguageconverter/Database.java
@@ -1,6 +1,68 @@
 package com.example.signlanguageconverter;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Database {
 
+	//multithreading shouldn't be necessary but just in case
+	private ConcurrentHashMap<String,String> data = new ConcurrentHashMap<>();
 
+
+	public String add(String text) {
+		AbstractMap.SimpleEntry<String ,String> entry = getEntry(text);
+		if(entry != null){
+			data.put(entry.getKey(),entry.getValue());
+			return entry.getKey() + " -> " + entry.getValue();
+		}
+		return null;
+	}
+
+	public void clear() {
+		data.clear();
+	}
+	// https://teckensprakslexikon.su.se/sok/idnummer?q=08028%2C04748%2C00529%2C02238%2C00179%2C11320%2C00857%2C03591
+	public String getLexicon() {
+		if(data.isEmpty()) return null;
+		StringBuilder builder = new StringBuilder();
+		builder.append("https://teckensprakslexikon.su.se/sok/idnummer?q=");
+		for(String value: data.values()){
+			builder.append(value).append("%2C");
+		}
+		return builder.toString();
+	}
+
+	public String getImportText() {
+		if(data.isEmpty()) return null;
+		StringBuilder builder = new StringBuilder();
+		builder.append("{\"name\":\"nameOfQuiz\",\"list\":[");
+		for(String value : data.values()){
+			builder.append("\"").append(value).append("\"");
+		}
+		builder.append("]}");
+		return builder.toString();
+	}
+
+	public String remove(String text) {
+		AbstractMap.SimpleEntry<String ,String> entry = getEntry(text);
+		if(entry != null && data.containsKey(entry.getKey())){
+			data.remove(entry.getKey());
+			return mapText();
+		}
+		return null;
+	}
+
+	private String mapText() {
+		StringBuilder builder = new StringBuilder();
+		for (Map.Entry<String, String> entry: data.entrySet()) {
+			builder.append(entry.getKey()).append(" -> ").append(entry.getValue());
+		}
+		return builder.toString();
+	}
+
+	private AbstractMap.SimpleEntry<String,String> getEntry(String inputlink){
+		//todo
+		return null;
+	}
 }
--- src/main/java/com/example/signlanguageconverter/SignLanguageConverterController.java
+++ src/main/java/com/example/signlanguageconverter/SignLanguageConverterController.java
@@ -1,8 +1,6 @@
 package com.example.signlanguageconverter;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -15,23 +13,46 @@ public class SignLanguageConverterController {
 	@FXML
 	private TextArea result;
 
+	@FXML
 	protected void onAddButtonClick(){
+		String resulting = database.add(input.getText());
+		if(resulting!=null) {
+			current.appendText(resulting);
+			input.clear();
+			input.setPromptText("link successfully added");
+		}
 
 	}
-
+	@FXML
 	protected void onRemoveButtonClick(){
+		String resulting = database.remove(input.getText());
+		if(resulting != null){
+			current.setText(resulting);
+			input.clear();
+			input.setPromptText("link successfully removed");
+		}
 
 	}
-
-	protected void onClearButtonClick(){
+	@FXML
+	protected void onRemoveAllDataButtonClick(){
+		database.clear();
+		current.clear();
 
 	}
-
+	@FXML
 	protected void onConvertToLexikonButtonClick(){
+		String result = database.getLexicon();
+		if(result!=null){
+			this.result.setText(result);
+		}
 
 	}
+	@FXML
 	protected void onConvertToImportButtonClick(){
-
+		String result = database.getImportText();
+		if(result!=null){
+			this.result.setText(result);
+		}
 	}
 
 
